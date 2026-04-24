f958ad8c38b7227f76bf6e319c7ff367517f8a77
Sry kimsour <soursour3001@gmail.com>
2026-04-24 10:33:23 +0700

    clean all material  test

--- src/app/test/material-test3/material-test3.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-material-test3',
-  imports: [],
-  templateUrl: './material-test3.html',
-  styleUrl: './material-test3.css',
-})
-export class MaterialTest3 {}
